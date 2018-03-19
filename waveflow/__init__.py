cf975c87f1a68b54e6e846d41c0ed070f1325390
pjarosik <jarosik.piotr@gmail.com>
2018-03-19 15:50:51 +0100

    Extracted transforms module.

--- waveflow/__init__.py
+++ waveflow/__init__.py
@@ -1,2 +1,5 @@
-from waveflow.python.ops.filters import *
-from waveflow.python.ops.beamforming import *
+import waveflow.python.envs as envs
+
+import waveflow.python.ops.filters as filters
+import waveflow.python.ops.transforms as transforms
+import waveflow.python.ops.beamforming as beamforming
---
+++ waveflow/python/ops/transforms.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import waveflow.python.op_util as op_util
+
+
+def hilbert(input, dt=1, name=None):
+  """
+  Computes Hilbert transform of complex-valued signal over the inner-most
+  dimension of input.
+
+  scipy compatibility
+  Equivalent to (-1)*scipy.fftpack.hilbert(input)
+
+  :param input: tensor of tf.complex64 values
+  :param dt: time step between consecutive samples
+  :return: tensor with values of type tf.complex64
+  """
+  with tf.name_scope(name, op_util.resolve_op_name("Hilbert"), [input]):
+    input = tf.convert_to_tensor(value=input, name="input", dtype=tf.complex64)
+    input_shape = tf.shape(input)
+    input_rank = tf.rank(input)
+    with tf.control_dependencies([
+      tf.assert_greater(input_rank, 0, data=[input],
+                        message="input can not be a scalar")
+    ]):
+      n = input_shape[input_rank - 1]
+      y_f = tf.fft(input)
+      f = tf.cast(fftfreq(n, d=dt), dtype=tf.complex64)
+      return tf.ifft(-1j * tf.sign(f) * y_f)
+
+
+def fftfreq(n, d=1.0, name=None):
+  """
+  Returns tf.fft sample frequencies.
+
+  numpy compatibility
+  Equivalent to np.fft.fftfreq
+
+  :param n: 0-D tensor - number of samples of signal in time-domain
+  :param d: value - sample spacing
+  :return: 1-D tensor of length n with sample frequencies.
+  """
+  with tf.name_scope(name, op_util.resolve_op_name("Fftfreq"), [n]):
+    n = tf.cast(n, dtype=tf.float32)
+    with tf.control_dependencies([
+      tf.assert_equal(tf.rank(n), 0, data=[n], message="n must be a scalar."),
+      tf.assert_greater_equal(n, 2., data=[n], message="n must be > 2")
+    ]):
+      T = d * n
+      dtype = tf.float32
+      non_negative_range = tf.cond(
+        tf.equal(tf.mod(n, 2), 0),
+        true_fn=lambda: tf.range(start=0, limit=n // 2, dtype=dtype),
+        false_fn=lambda: tf.range(start=0, limit=((n - 1) // 2) + 1,
+                                  dtype=dtype)
+      )
+      negative_range = tf.cond(
+        tf.equal(tf.mod(n, 2), 0),
+        true_fn=lambda: tf.range(start=-1 * (n // 2), limit=0, dtype=dtype),
+        false_fn=lambda: tf.range(start=-1 * ((n - 1) // 2), limit=0,
+                                  dtype=dtype)
+      )
+      return tf.concat(
+        [tf.div(non_negative_range, T), tf.div(negative_range, T)], -1)
+
+
