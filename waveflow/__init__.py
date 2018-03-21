b3242207c4887cf9eca24c5ad51c08fa7bd460e4
pjarosik <jarosik.piotr@gmail.com>
2018-03-21 08:39:21 +0100

    Added math, signal and units ops modules to bazel project.

--- waveflow/__init__.py
+++ waveflow/__init__.py
@@ -1,6 +1,9 @@
 from waveflow.python.ops import filters
 from waveflow.python.ops import transforms
 from waveflow.python.ops import beamforming
+from waveflow.python.ops.math_ops import *
+import waveflow.python.ops.signal_ops as signal
+import waveflow.python.ops.units as units
 
 from waveflow.python.envs import io
 from waveflow.python.envs import physics
